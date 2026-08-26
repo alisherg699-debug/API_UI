5565224e9d2c387dd7ea7532386e1ccefb3ab4d7
imvco <imvco@example.com>
2026-08-26 12:26:53 +0500

    kod o'zgartirildi

--- lib/bloc/character/character_bloc.dart
+++ lib/bloc/character/character_bloc.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../repositories/character_repository.dart';
+import '../../models/character_model.dart';
 import 'character_event.dart';
 import 'character_state.dart';
 
@@ -13,10 +14,28 @@ class CharacterBloc extends Bloc<CharacterEvent, CharacterState> {
     FetchCharacters event,
     Emitter<CharacterState> emit,
   ) async {
-    emit(CharacterLoading());
+    final currentState = state;
+    List<Character> oldCharacters = [];
+
+    if (event.page == 1) {
+      emit(CharacterLoading());
+    }
+    else if (currentState is CharacterLoaded) {
+      oldCharacters = currentState.characterResponse.results;
+    }
     try {
       final result = await repository.getCharacters(event.page);
-      emit(CharacterLoaded(result));
+
+      if (event.page > 1) {
+        emit(CharacterLoaded(
+          CharacterResponse(
+            info: result.info,
+            results: oldCharacters + result.results,
+          ),
+        ));
+      } else {
+        emit(CharacterLoaded(result));
+      }
     } catch (e) {
       emit(CharacterError(e.toString()));
     }
