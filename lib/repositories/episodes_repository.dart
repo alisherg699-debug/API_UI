0b801ef27ccdb83fbc28798a5acaaaa0b55dd819
imvco <imvco@example.com>
2026-08-28 11:12:07 +0500

    fix

--- lib/repositories/episodes_repository.dart
+++ lib/repositories/episodes_repository.dart
@@ -1,15 +1,21 @@
 import 'dart:convert';
+import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
 import '../models/episodes_model.dart';
 
 class EpisodesRepository {
   Future<EpisodesResponse> getEpisodes(int page) async {
+    debugPrint('------------------------------------------------------------------------');
+    debugPrint('🚀 Request URL: https://rickandmortyapi.com/api/episode/?page=$page');
+    debugPrint('------------------------------------------------------------------------');
     final response = await http.get(
       Uri.parse('https://rickandmortyapi.com/api/episode/?page=$page'),
     );
 
     if (response.statusCode == 200) {
-      print("📥 GET response: ${response.body}");
+      debugPrint('----------------------------------------------');
+      debugPrint("✅ Ma'lumot keldi. Sahifa: $page");
+      debugPrint('----------------------------------------------');
       return EpisodesResponse.fromJson(jsonDecode(response.body));
     } else {
       throw Exception('Maʼlumotlarni yuklashda xatolik yuz berdi');
